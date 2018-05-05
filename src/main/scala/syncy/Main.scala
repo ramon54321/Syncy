b888562e5b427288f6149035fa20eb443a3a8c9c
anton5 <anton5@users.noreply.github.com>
2018-05-05 15:59:14 +0300

    Changed the formatting.

--- src/main/scala/syncy/Main.scala
+++ src/main/scala/syncy/Main.scala
@@ -2,8 +2,12 @@ package syncy
 
 object Main {
 	def main(args : Array[String]) : Unit = {
-		if (args.length < 1) return
-		else if (args(0).toLowerCase() == "cli") CliMain()
-		else if (args(0).toLowerCase() == "server") ServerMain()
+		if (args.length < 1) {
+			return
+		} else if (args(0).toLowerCase() == "cli") {
+			CliMain()
+		} else if (args(0).toLowerCase() == "server") {
+			ServerMain()
+		}
 	}
 }
