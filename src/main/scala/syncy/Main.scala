d548e06005c811b9420f337577e37d48a06f9c7a
Anton <anton5@users.noreply.github.com>
2018-05-09 14:08:36 -0400

    A more correct handling of command line arguments.

--- src/main/scala/syncy/Main.scala
+++ src/main/scala/syncy/Main.scala
@@ -1,13 +1,19 @@
 package syncy
 
 object Main {
-	def main(args : Array[String]) : Unit = {
-		if (args.length < 1) {
-			return
-		} else if (args(0).toLowerCase() == "cli") {
-			CliMain()
-		} else if (args(0).toLowerCase() == "server") {
-			ServerMain(args(1)) // -- Start server at port
-		}
-	}
+    def main(args : Array[String]) : Unit = {
+        if (args.length == 1 && args(0).toLowerCase() == "cli") {
+            CliMain()
+        } else if (args.length == 2 && args(0).toLowerCase() == "server") {
+            ServerMain(args(1)) // -- Start server at port
+        } else {
+            print(
+            """Usage:
+              |syncy server <port>
+              |syncy cli
+              |""".stripMargin
+            )
+        }
+    }
 }
+
